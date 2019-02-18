e096112525e206a81a8f16054e6606a124374677
itsfootball <julianbici@gmail.com>
2019-02-17 22:09:46 -0500

    test

--- vars/tfInit.groovy
+++ vars/tfInit.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String key = 's3_key_here') {
-  /usr/local/bin/terraform init -no-color -backend-config="terraform-state"
+  /usr/local/bin/terraform init -no-color -backend-config="key=terraform-state/tfstate"
 }
