dd0d0b5de4910d4cf673f7037198d86d4733efc2
itsfootball <julianbici@gmail.com>
2019-02-18 10:08:38 -0500

    test

--- vars/awsAUTH.groovy
+++ vars/awsAUTH.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String region) {
     withCredentials([[
       $class: 'AmazonWebServicesCredentialsBinding',
       credentialsId: 'aws_credentials',
