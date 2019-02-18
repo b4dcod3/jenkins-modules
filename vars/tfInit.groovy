ff8eb113bdec3979ec8f9d306b13925088c32466
itsfootball <julianbici@gmail.com>
2019-02-18 18:18:50 -0500

    add bucket

--- vars/tfInit.groovy
+++ vars/tfInit.groovy
@@ -1,9 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String key, String region) {
+def call(String bucket, String key, String region) {
   sh 
   """
   terraform init -no-color \
+  -backend-config="bucket=${bucket}"\
   -backend-config="key=${key}"\
   -backend-config="region=${region}"
   echo "[DEBUG] terraform init complete" 
