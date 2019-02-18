ca039328f1da70ddb18f9e3e99de9b3943ca59e5
itsfootball <julianbici@gmail.com>
2019-02-17 23:10:18 -0500

    add aws credentials

--- vars/describeEC2.groovy
+++ vars/describeEC2.groovy
@@ -1,5 +1,14 @@
 #!/usr/bin/env groovy
 
 def call(String region = 'us-east-1') {
-  sh "aws ec2 describe-instances --region ${region}"
+    withCredentials([[
+      $class: 'AmazonWebServicesCredentialsBinding',
+      credentialsId: 'aws_credentials',
+      accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+      ]]) {             
+       sh "aws ec2 describe-instances --region ${region}"
+      }
+  } // withCredentials
+  
 }
