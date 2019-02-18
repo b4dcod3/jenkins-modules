f1a937ce3fd763f6ce57155ba460b26efae2bd6c
itsfootball <julianbici@gmail.com>
2019-02-18 16:33:20 -0500

    test

--- vars/awsAUTH.groovy
+++ vars/awsAUTH.groovy
@@ -6,13 +6,13 @@ def call(String region) {
       credentialsId: 'aws_credentials',
       accessKeyVariable: 'AWS_ACCESS_KEY_ID',
       secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
-      ]]){             
-            sh 
-            """
-            aws configure set aws_access_key_id `echo \${AWS_ACCESS_KEY_ID}`
-            aws configure set aws_secret_access_key `echo \${AWS_SECRET_ACCESS_KEY}`
+      ]]) {             
+      sh 
+      """
+      aws configure set aws_access_key_id `echo \${AWS_ACCESS_KEY_ID}`
+      aws configure set aws_secret_access_key `echo \${AWS_SECRET_ACCESS_KEY}`
 
-            """
+      """
       }
   
 }
