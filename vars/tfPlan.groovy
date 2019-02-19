e68d6ed9e84d30d6c257abe47420ad19796a9c77
itsfootball <julianbici@gmail.com>
2019-02-18 20:14:31 -0500

    no detail exit code

--- vars/tfPlan.groovy
+++ vars/tfPlan.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String key) {
-  sh "terraform plan -detailed-exitcode -no-color -detailed-exitcode 0 -out ${key}.tfplan"
+  sh "terraform plan -no-color -out ${key}.tfplan"
 }
